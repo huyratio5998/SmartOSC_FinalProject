0c05a0f3eeed1ad84ffd9836b39dd725a9e26324
PhiKien <44530636+PhiKien@users.noreply.github.com>
2019-07-02 15:17:07 +0700

    Update

---
+++ Final_Project/Final_Project/Models/IdentityModels.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Final_Project.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string UrlAvatar { get; set; }
+
+        [Required, StringLength(32, MinimumLength = 2)]
+        public string FullName { get; set; }
+
+        public int ProjectId { get; set; }
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<Project> Prj { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("ManageProjectDB", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
